302e27198a5501955fb277bff6aa02bb4ecd64f1
Ethan Gregory Clark <ethangclark@gmail.com>
2024-12-02 16:33:50 -0700

    tree with pct display

--- src/app/enrollment/[id]/page.tsx
+++ src/app/enrollment/[id]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Spin } from "antd";
+import { Spin, Tree, type TreeDataNode } from "antd";
 import { useMemo } from "react";
 import { z } from "zod";
 import { ClientOnly } from "~/app/_components/ClientOnly";
@@ -13,12 +13,23 @@ type Props = {
   };
 };
 
+const toPct = (numerator: number, denominator: number) =>
+  Math.floor((numerator / denominator) * 100);
+const addPct = (title: string, pct: number) => `${title} - ${pct}%`;
+
 export default function CoursePage({ params }: Props) {
   const enrollmentId = z.string().parse(params.id);
   const enrollment = api.course.enrollment.useQuery({ enrollmentId });
   const activites = api.activity.enrollmentActivities.useQuery({
     enrollmentId,
   });
+
+  const completedTopicIds = useMemo((): Set<string> => {
+    if (!activites.data) {
+      return new Set();
+    }
+    return new Set(activites.data.map((a) => a.topicId));
+  }, [activites.data]);
 
   const pctDone = useMemo(() => {
     let total = 0;
@@ -27,14 +38,51 @@ export default function CoursePage({ params }: Props) {
         total += module.topics.length;
       });
     });
-    let completed = 0;
-    activites.data?.forEach((activity) => {
-      if (activity.topicMasteryProved) {
-        completed++;
-      }
-    });
-    return Math.floor((completed / total) * 100);
-  }, [activites.data, enrollment.data?.course.units]);
+    return toPct(completedTopicIds.size, total);
+  }, [completedTopicIds.size, enrollment.data?.course.units]);
+
+  const treeData = useMemo((): TreeDataNode[] => {
+    if (!enrollment.data) {
+      return [];
+    }
+    return [
+      {
+        title: addPct(enrollment.data.course.courseType.name, pctDone),
+        key: enrollment.data.course.id,
+        children: enrollment.data.course.units.map((unit) => {
+          let done = 0;
+          let total = 0;
+          unit.modules.forEach((module) => {
+            module.topics.forEach((topic) => {
+              total++;
+              if (completedTopicIds.has(topic.id)) {
+                done++;
+              }
+            });
+          });
+          return {
+            title: addPct(unit.name, toPct(done, total)),
+            key: unit.id,
+            children: unit.modules.map((module) => ({
+              title: addPct(
+                module.name,
+                toPct(
+                  module.topics.filter((t) => completedTopicIds.has(t.id))
+                    .length,
+                  module.topics.length,
+                ),
+              ),
+              key: module.id,
+              children: module.topics.map((topic) => ({
+                title: topic.name,
+                key: topic.id,
+              })),
+            })),
+          };
+        }),
+      },
+    ];
+  }, [completedTopicIds, enrollment.data, pctDone]);
 
   if (!enrollment.data || !activites.data) {
     return <Spin />;
@@ -44,7 +92,7 @@ export default function CoursePage({ params }: Props) {
     <Page>
       <Title>Course enrollment: {enrollmentId}</Title>
       <ClientOnly>
-        <div>Pct done: {pctDone}%</div>
+        <Tree treeData={treeData} />
       </ClientOnly>
     </Page>
   );
