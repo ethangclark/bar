08d6a1961eb81fe1962ce812ce2d7a0d483109ba
Ethan Gregory Clark <ethangclark@gmail.com>
2024-12-02 20:45:25 -0700

    title

--- src/app/enrollment/[id]/topic.tsx
+++ src/app/enrollment/[id]/topic.tsx
@@ -3,7 +3,14 @@ import { type TopicContext } from "~/server/db/schema";
 export function Topic({ topicContext }: { topicContext: TopicContext }) {
   const { course, courseType, unit, module, topic } = topicContext;
   return (
-    <div className="h-full w-full px-8" style={{ width: 672 }}>
+    <div
+      className="mb-2 flex h-full w-full flex-col px-8"
+      style={{ width: 672 }}
+    >
+      <div>
+        {courseType.name} &gt; {unit.name} &gt; {module.name}
+      </div>
+      <div className="mb-4 text-2xl">{topic.name}</div>
       <div className="outline-3 h-full w-full rounded-3xl p-8 outline outline-gray-200">
         <h1>{courseType.name}</h1>
         <h2>{course.id}</h2>
