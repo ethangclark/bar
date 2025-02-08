fa8b193456643465511b9b147012620daa25f4de
Ethan Gregory Clark <ethangclark@gmail.com>
2025-02-08 14:34:23 -0700

    wire up LLM

--- src/server/services/summitService.ts
+++ src/server/services/summitService.ts
@@ -1,12 +1,37 @@
+import { eq } from "drizzle-orm";
+import { db, DbOrTx } from "../db";
 import { messageDeltaPubSub } from "../db/pubsub/messageDeltaPubSub";
 import { Message } from "../db/schema";
+import { streamLlmResponse } from "../ai/llm";
+
+const model = "deepseek/deepseek-chat";
 
 export async function streamResponse(toMessage: Message) {
-  console.log("toMessage");
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  messageDeltaPubSub.publish({
-    activityId: toMessage.activityId,
-    messageId: toMessage.id,
-    contentDelta: "Hello, world!94333",
+  let messages = await db.query.messages.findMany({
+    where: eq(db.x.messages.threadId, toMessage.threadId),
   });
+  messages = messages
+    .sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime())
+    .filter((m) => m.id !== toMessage.id);
+
+  const gen = streamLlmResponse(
+    toMessage.userId,
+    {
+      model,
+      messages: messages.map((m) => ({
+        role: m.senderRole,
+        content: m.content,
+      })),
+    },
+    db,
+  );
+  for await (const resp of gen) {
+    if (typeof resp === "string") {
+      messageDeltaPubSub.publish({
+        activityId: toMessage.activityId,
+        messageId: toMessage.id,
+        contentDelta: resp,
+      });
+    }
+  }
 }
