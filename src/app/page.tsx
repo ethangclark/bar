ee30cdb5a9fa3aefeec98ba0c9cc65f338bb1429
Ethan Gregory Clark <ethangclark@gmail.com>
2025-02-17 13:51:33 -0700

    sign up/in links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "antd";
+import { Button, Dropdown } from "antd";
 import Link from "next/link";
 import { Image } from "~/client/components/Image";
 import { Logo, LogoText } from "~/client/components/Logo";
@@ -9,50 +9,68 @@ import { Page } from "~/client/components/Page";
 export default function Home() {
   return (
     <Page>
-      <div className="mb-5 flex gap-24">
+      <div className="mb-5 flex w-full items-center justify-between">
         <div className="flex flex-col items-center">
           <div className="flex items-center">
             <Logo height={50} />
-            <LogoText className="text-4xl md:text-6xl" />
+            <LogoText className="text-4xl md:text-5xl" />
           </div>
-          <h1 className="text-lg font-bold md:text-2xl">
+          <h1 className="text-lg font-bold md:text-xl">
             AI Learning Assistant
           </h1>
         </div>
         <div className="flex gap-4">
-          <Button type="primary">Instructors</Button>
-          <Button type="primary">Students</Button>
+          <Dropdown
+            menu={{
+              items: [
+                {
+                  key: "student",
+                  label: <Link href="/signin/student">For students</Link>,
+                },
+                {
+                  key: "instructors",
+                  label: <Link href="/signin/instructor">For instructors</Link>,
+                },
+              ],
+            }}
+          >
+            <Button type="primary" size="large">
+              Sign up
+            </Button>
+          </Dropdown>
+          <Dropdown
+            menu={{
+              items: [
+                {
+                  key: "students",
+                  label: <Link href="/signin/student">For students</Link>,
+                },
+                {
+                  key: "instructors",
+                  label: <Link href="/signin/instructor">For instructors</Link>,
+                },
+              ],
+            }}
+          >
+            <Button size="large">Sign in</Button>
+          </Dropdown>
         </div>
       </div>
       <div className="flex flex-grow flex-col items-center justify-between">
         <div className="mb-4 flex flex-grow flex-col items-center justify-center md:gap-8">
-          <h1 className="mb-[-24px] text-center text-4xl font-bold">
-            Raising student achievement. Dramatically.
-          </h1>
-          <h2 className="text-center text-xl">
-            Tutored student activities for less than the cost of a textbook
-          </h2>
-          <Image url="/images/bloom.png" alt="Hero" width={750} />
-          <div className="flex gap-4">
-            <a
-              href="/api/auth/signin"
-              className="rounded-full bg-blue-500 px-10 py-3 font-semibold text-white no-underline transition hover:bg-blue-600"
-            >
-              Sign up
-            </a>
-            <a
-              href="/api/auth/signin"
-              className="rounded-full bg-gray-300 px-10 py-3 font-semibold no-underline transition hover:bg-blue-200"
-            >
-              Sign in
-            </a>
-            <Link
-              href={"/api/auth/signout"}
-              className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-            >
-              {"Sign out"}
-            </Link>
+          <div className="flex flex-col items-center gap-4">
+            <h1 className="text-center text-4xl font-bold">
+              Dramatically improve student achievement
+            </h1>
+            <h2 className="text-center text-xl">
+              Tutored student activities for less than the cost of a textbook
+            </h2>
           </div>
+          <Image
+            url="/images/bloom.png"
+            alt="Bloom's research showing a massive increase in student achievement with the use of tutorial education"
+            width={750}
+          />
         </div>
         <div className="text-center text-sm text-gray-500">
           Questions? Comments? Email us at hello@summited.ai
