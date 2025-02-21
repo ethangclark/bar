8d92c671afe1b55bdab580e91b7e1ea3673c4ab8
Ethan Gregory Clark <ethangclark@gmail.com>
2025-02-20 20:41:22 -0700

    HOST_NAME support

--- src/common/urlUtils.ts
+++ src/common/urlUtils.ts
@@ -1,7 +1,11 @@
 export function getBaseUrl() {
   if (typeof window !== "undefined") return window.location.origin;
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-  return `http://localhost:${process.env.PORT ?? 4000}`;
+  const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
+  const hostname = process.env.HOST_NAME;
+  const port =
+    process.env.NODE_ENV === "production" ? "" : `:${process.env.PORT ?? 4000}`;
+  return `${protocol}://${hostname}${port}`;
 }
 
 export function getTrpcUrl() {
