eec8ba256a5195c47d97aedec313b7d7dc04e72e
Ethan Gregory Clark <ethangclark@gmail.com>
2025-03-10 20:06:01 -0600

    next nonsense

--- src/app/login/student/page.tsx
+++ src/app/login/student/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 
+import { Suspense } from "react";
+import { LoadingCentered } from "~/client/components/Loading";
 import { LoginPage } from "~/client/components/LoginPage";
 
 export default function LogInStudent() {
-  return <LoginPage />;
+  return (
+    // I hate Next.js
+    <Suspense fallback={<LoadingCentered />}>
+      <LoginPage />
+    </Suspense>
+  );
 }
