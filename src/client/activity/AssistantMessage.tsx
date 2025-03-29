edee868c211f28261b276e2c9918ec9d6dbf7916
Ethan Gregory Clark <ethangclark@gmail.com>
2025-03-29 14:34:34 -0600

    display the base system message alongside child messages if diagnostics is enabled

--- src/client/activity/AssistantMessage.tsx
+++ src/client/activity/AssistantMessage.tsx
@@ -49,6 +49,7 @@ export const AssistantMessage = storeObserver<{
   scrollToBottom,
   viewPieceStore,
   descendentStore,
+  diagnosticsStore,
 }) {
   const children = viewPieceStore.viewPieceChildren(message.id);
 
@@ -60,87 +61,86 @@ export const AssistantMessage = storeObserver<{
     return flags.find((f) => f.messageId === message.id) ?? null;
   }, [flags, message.id]);
 
-  if (children) {
-    return (
-      <>
-        {children.map((child, i) => {
-          const isLastChild = i === children.length - 1;
-          const isLast = isLastMessage && isLastChild;
-          switch (child.type) {
-            case "image":
-              return (
-                <MessageView
-                  key={child.key}
-                  id={isLastChild ? message.id : undefined}
-                  isLastMessage={isLast}
-                  messageLength={child.url.length}
-                  scrollToBottom={scrollToBottom}
-                  flag={isLastChild ? flag : null}
-                  diagnosticMessage="ASSISTANT MESSAGE - IMAGE"
-                >
-                  <Image alt={child.textAlternative} url={child.url} />
-                </MessageView>
-              );
-            case "video":
-              return (
-                <MessageView
-                  key={child.key}
-                  id={isLastChild ? message.id : undefined}
-                  isLastMessage={isLast}
-                  messageLength={child.textAlternative.length}
-                  scrollToBottom={scrollToBottom}
-                  flag={isLastChild ? flag : null}
-                  diagnosticMessage="ASSISTANT MESSAGE - VIDEO"
-                >
-                  <Video videoId={child.videoId} />
-                </MessageView>
-              );
-            case "text":
-              return (
-                <MessageView
-                  key={child.key}
-                  id={isLastChild ? message.id : undefined}
-                  isLastMessage={isLast}
-                  messageLength={child.content.length}
-                  scrollToBottom={scrollToBottom}
-                  flag={isLastChild ? flag : null}
-                  diagnosticMessage="ASSISTANT MESSAGE - TEXT"
-                >
-                  <PreformattedText>{child.content}</PreformattedText>
-                </MessageView>
-              );
-            default:
-              assertTypesExhausted(child);
-          }
-        })}
-      </>
-    );
-  } else {
-    return (
-      <MessageView
-        id={message.id}
-        isLastMessage={isLastMessage}
-        messageLength={message.content.length}
-        scrollToBottom={scrollToBottom}
-        flag={flag}
-        diagnosticMessage="ASSISTANT MESSAGE"
-        footer={
-          message.status === "incomplete" ? (
-            <div className="bg-white text-gray-600">
-              Thinking
-              <FancyEllipsis />
-            </div>
-          ) : undefined
+  const { diagnosticsEnabled } = diagnosticsStore;
+
+  return (
+    <>
+      {children?.map((child, i) => {
+        const isLastChild = i === children.length - 1;
+        const isLast = isLastMessage && isLastChild;
+        switch (child.type) {
+          case "image":
+            return (
+              <MessageView
+                key={child.key}
+                id={isLastChild ? message.id : undefined}
+                isLastMessage={isLast}
+                messageLength={child.url.length}
+                scrollToBottom={scrollToBottom}
+                flag={isLastChild ? flag : null}
+                diagnosticMessage="ASSISTANT MESSAGE - IMAGE"
+              >
+                <Image alt={child.textAlternative} url={child.url} />
+              </MessageView>
+            );
+          case "video":
+            return (
+              <MessageView
+                key={child.key}
+                id={isLastChild ? message.id : undefined}
+                isLastMessage={isLast}
+                messageLength={child.textAlternative.length}
+                scrollToBottom={scrollToBottom}
+                flag={isLastChild ? flag : null}
+                diagnosticMessage="ASSISTANT MESSAGE - VIDEO"
+              >
+                <Video videoId={child.videoId} />
+              </MessageView>
+            );
+          case "text":
+            return (
+              <MessageView
+                key={child.key}
+                id={isLastChild ? message.id : undefined}
+                isLastMessage={isLast}
+                messageLength={child.content.length}
+                scrollToBottom={scrollToBottom}
+                flag={isLastChild ? flag : null}
+                diagnosticMessage="ASSISTANT MESSAGE - TEXT"
+              >
+                <PreformattedText>{child.content}</PreformattedText>
+              </MessageView>
+            );
+          default:
+            assertTypesExhausted(child);
         }
-      >
-        <PreformattedText
-          className={
-            message.status === "incomplete" ? "text-xs text-gray-300" : ""
+      })}
+      {(!children || diagnosticsEnabled) && (
+        <MessageView
+          id={message.id}
+          isLastMessage={isLastMessage}
+          messageLength={message.content.length}
+          scrollToBottom={scrollToBottom}
+          flag={flag}
+          diagnosticMessage="ASSISTANT MESSAGE - BASE"
+          footer={
+            message.status === "incomplete" ? (
+              <div className="bg-white text-gray-600">
+                Thinking
+                <FancyEllipsis />
+              </div>
+            ) : undefined
           }
         >
-          {message.content}
-        </PreformattedText>
-      </MessageView>
-    );
-  }
+          <PreformattedText
+            className={
+              message.status === "incomplete" ? "text-xs text-gray-300" : ""
+            }
+          >
+            {message.content}
+          </PreformattedText>
+        </MessageView>
+      )}
+    </>
+  );
 });
