c5351871313217a0eae29d48f5761b93456f90a0
Ethan Gregory Clark <ethangclark@gmail.com>
2025-03-29 21:11:57 -0600

    clarify prompt test

--- vitest.promptTest.config.ts
+++ vitest.promptTest.config.ts
@@ -1,11 +1,11 @@
+import { create } from "mutative";
 import { defineConfig } from "vitest/config";
 import { defaultConfig, promptTestsPattern } from "./vitest.config";
-import { create } from "mutative";
 
 const asDefineParam: Parameters<typeof defineConfig>[0] = defaultConfig;
 
-const testTimeout = process.env.TEST_TIMEOUT
-  ? parseInt(process.env.TEST_TIMEOUT)
+const testTimeout = process.env.TEST_TIMEOUT_SECONDS
+  ? parseInt(process.env.TEST_TIMEOUT_SECONDS) * 1000
   : 30000;
 
 const config = create(asDefineParam, (draft) => {
